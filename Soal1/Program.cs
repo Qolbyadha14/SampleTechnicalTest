c7ee11cee53c77e5ff8b777c3fd02e5f7600acd5
qolby.adha <qolby.adha@kawanlamacorp.com>
2023-10-14 16:12:12 +0700

    Add project files.

---
+++ Soal1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int jumlahPenumpang = 0;
+        int jumlahMinibusTeralokasi = 0;
+
+        Console.WriteLine("Input jumlah keluarga : ");
+        int jumlahKeluarga = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input anggota keluarga : ");
+        string[] anggotaKeluarga = Console.ReadLine().Split(' ');
+
+        if (anggotaKeluarga.Length != jumlahKeluarga)
+        {
+            Console.WriteLine("Input must be equal with count of family");
+            return;
+        }
+
+        int iterator = 0;
+
+        while (iterator < jumlahKeluarga)
+        {
+            jumlahPenumpang += int.Parse(anggotaKeluarga[iterator]);
+            iterator++;
+        }
+
+        if (jumlahPenumpang % 4 == 0)
+        {
+            jumlahMinibusTeralokasi = jumlahPenumpang / 4;
+        }
+        else if (jumlahPenumpang > 0)
+        {
+            jumlahMinibusTeralokasi = (jumlahPenumpang / 4) + 1;
+        }
+
+        Console.WriteLine("Minimum bus required is : " + jumlahMinibusTeralokasi);
+    }
+}
---
+++ Soal2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input string : ");
+        string input = Console.ReadLine();
+
+        string charVowel = ProcessCharacters(procVowel(input));
+        string charConsonant = ProcessCharacters(procConsonant(input));
+
+        Console.WriteLine("Vowel Characters : ");
+        Console.WriteLine(charVowel);
+        Console.WriteLine("Consonant Characters : ");
+        Console.WriteLine(charConsonant);
+    }
+
+    public static string ProcessCharacters(string input)
+    {
+        string processedInput = new string(input.Where(char.IsLetter).Select(char.ToLower).ToArray());
+
+        var groupedCharacters = processedInput.GroupBy(c => c).Select(group => new { Char = group.Key, Count = group.Count() });
+
+        StringBuilder result = new StringBuilder();
+        foreach (var group in groupedCharacters)
+        {
+            result.Append(new string(group.Char, group.Count));
+        }
+
+        return result.ToString();
+    }
+
+    public static string procVowel(string param)
+    {
+        // Selecting only vowel characters
+        return new string(param.Where(c => "aeiou".IndexOf(c) != -1).ToArray());
+    }
+
+    public static string procConsonant(string param)
+    {
+        // Selecting only consonant characters
+        return new string(param.Where(c => "aeiou".IndexOf(c) == -1).ToArray());
+    }
+}
